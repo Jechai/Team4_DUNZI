293ba438d80c2dc0f5d0034444db1c37ec5fe5fa
Jechai <44994182+Jechai@users.noreply.github.com>
2019-11-14 11:06:13 +0800

    Update HomeController.cs

--- AspNetCoreWebApplication/Controllers/HomeController.cs
+++ AspNetCoreWebApplication/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+/*using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -21,4 +21,4 @@ namespace AspNetCoreWebApplication.Controllers
             return View();
         }
     }
-}
+}*/
